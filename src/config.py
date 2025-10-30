041f6d6ffcbe11cb43003819e44d3fea270ad3d3
yohakuo <2394791478@qq.com>
2025-10-30 17:29:53 +0800

    Vault backup

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ CLICKHOUSE_SHARED_HOST = "192.168.121.57"
 CLICKHOUSE_SHARED_PORT = "5050"
 CLICKHOUSE_SHARED_USER = "root"
 CLICKHOUSE_SHARED_PASSWORD = "123456"
-CLICKHOUSE_SHARED_DB = "test_data"  # original_data
+CLICKHOUSE_SHARED_DB = "original_data"  # test_data
 CLICKHOUSE_MEASUREMENT_NAME = "sensor_temp_humidity"  # sensor_co2
 
 # ====================================================================
